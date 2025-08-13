ffc51510918112c80c055d16fbb953ef55516bb5
Muhibbulloh <225215017+muhib137@users.noreply.github.com>
2025-08-13 18:59:01 +0500

    Exercise 7: Reverse String Solution

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let reversedString = '';
+  const stringChars = string.split("");
+  for(let i = string.length - 1; i >= 0; i--){
+    reversedString += stringChars[i];
+  }
+  return reversedString;
 };
 
 // Do not edit below this line

55c09a28eb9c81bcd2643beaeaad4e0112b71247
Muhibbulloh <225215017+muhib137@users.noreply.github.com>
2025-08-14 11:58:23 +0500

    Exercise 9: Sum All Solution

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (start, finish) {
+  if (Number.isNaN(start + finish)) return "ERROR";
+  if (!Number.isInteger(start + finish)) return "ERROR";
+  if (start < 0 || finish < 0) return "ERROR";
 
+  let sum = 0;
+  if (start > finish) {
+    for (let i = finish; i <= start; i++) {
+      sum += i;
+    }
+  } else {
+    for (let i = start; i <= finish; i++) {
+      sum += i;
+    }
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
